a31a8a46711a342cd3366a820a94f1f0af3983cd
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-05-01 17:22:08 +0530

    test case for multiple ride

--- src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
@@ -1,5 +1,42 @@
 package com.bridgelabz.cabinvoicegeneratorTest;
 
-public class CabInvoiceGeneratorTest {
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
+import com.bridgelabz.cabinvoicegenerator.Ride;
+
+public class CabInvoiceGeneratorTest 
+{
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare()
+	{
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double distance = 5.0;
+		double time = 2;
+		double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+		assertEquals(52, totalfare,0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime_WhenLessThanMinimumFare_ShouldReturnTotalFare()
+	{
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double distance = 0.1;
+		double time = 2;
+		double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+		assertEquals(5, totalfare,0.0);
+	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare()
+	{
+		CabInvoiceGenerator cabInvoiceGenerator =new CabInvoiceGenerator();
+		Ride[] rides = { new Ride(5.0,2),new Ride(0.1,2)};
+		double totalfare =  cabInvoiceGenerator.calculateTotalFare(rides);
+		assertEquals(57, totalfare,0.0);
+	}
+	
 
 }
